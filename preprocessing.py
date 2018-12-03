1d18dd220a2ee23e2bb602121552e7185365ae25
okzapradhana <okzamahendra29@gmail.com>
2018-12-03 23:40:43 +0700

    return dataset total rows to use in document parse based on class later

--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,22 @@ import numpy as np
 import csv
 import pandas
 
-def readDocument():
-    df = pandas.read_csv('training_dataset.csv')
+def readDocument(documentName, columnName, nrows=10):
+    df = pandas.read_csv(documentName, nrows=nrows)
+    if(columnName == "none"):
+        return df
+    else:
+        return df[columnName]
+
+def writeDocument(documentName):
+    df = pandas.DataFrame.to_csv(documentName)
     print(df)
 
-readDocument()
+df = pandas.read_csv('training_dataset.csv')
+count = df.index
+
+print(len(count)
+
+document = readDocument('training_dataset.csv', "none")
+print(document)
+
